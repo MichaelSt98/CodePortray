be116436fce4649765eea2ef264fd90827c579e0
Michael Staneker <50531288+MichaelSt98@users.noreply.github.com>
2022-06-29 21:25:25 +0200

    Added docs (for testing purposes)

---
+++ docs/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['features_20of_20miluphcuda_20and_20their_20importance_2fpriority_0',['Features of miluphcuda and their importance/priority',['../md_resources_miluphcuda_physics_in_miluphcuda.html',1,'']]]
+];
---
+++ docs/search/pages_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sedov_20test_20case_0',['Sedov test case',['../md_testcases_sedov__r_e_a_d_m_e.html',1,'']]]
+];
